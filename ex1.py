24b484e24e6aaa6869f0393f4af49755483d5fdc
sarahd-s1 <61825838+sarahd-s1@users.noreply.github.com>
2021-05-03 16:56:01 +0300

    submited

--- ex1.py
+++ ex1.py
@@ -6,11 +6,11 @@ import re
 
 
 def dtw(mfcc1, mfcc2):
-    dtw_mat = np.zeros([32, 32], np.float64)
+    dtw_mat = np.zeros([101, 101], np.float64)
     euclid_mat = euclid_mat_helper(mfcc1, mfcc2)
     dtw_mat[0, 0] = euclid_mat[0, 0]
-    for i in range(32):
-        for j in range(32):
+    for i in range(101):
+        for j in range(101):
             if j == 0:
                 dtw_mat[i, j] = euclid_mat[i, j] + dtw_mat[i - 1, j]
             elif i == 0:
@@ -21,7 +21,7 @@ def dtw(mfcc1, mfcc2):
 
 
 def euclid_mat_helper(mfcc1, mfcc2):
-    euclid_mat = np.zeros([32, 32], np.float32)
+    euclid_mat = np.zeros([101, 101], np.float32)
     for i, vector1 in enumerate(mfcc1, 0):
         for j, vector2 in enumerate(mfcc2, 0):
             euclid_mat[i, j] = distance.euclidean(vector1, vector2)
@@ -60,20 +60,20 @@ def main():
         index += 1
     with open("output.txt", "w+") as pred:
         pred.write('\n'.join(str(v) for v in results_to_files))
-    directory_labeled = "labeled"
-    correct = 0
-    for root, subdirectories, all_files in os.walk(directory_labeled):
-        for subdirectory in subdirectories:
-            for filename in os.listdir("labeled/" + subdirectory):
-                with open('output.txt') as f:
-                    datafile = f.readlines()
-                for line in datafile:
-                    if filename in line:
-                        res = line.split('-')[2]
-                        res = re.sub(r"[\n\t\s]*", "", res)
-                        if res == subdirectory:
-                            correct += 1
-    print("\tTest set: Accuracy: {}/{} ({:.0f}%)".format(correct, 250, 100. * correct / 250))
+    # directory_labeled = "labeled"
+    # correct = 0
+    # for root, subdirectories, all_files in os.walk(directory_labeled):
+    #     for subdirectory in subdirectories:
+    #         for filename in os.listdir("labeled/" + subdirectory):
+    #             with open('output.txt') as f:
+    #                 datafile = f.readlines()
+    #             for line in datafile:
+    #                 if filename in line:
+    #                     res = line.split('-')[2]
+    #                     res = re.sub(r"[\n\t\s]*", "", res)
+    #                     if res == subdirectory:
+    #                         correct += 1
+    #print("\tTest set: Accuracy: {}/{} ({:.0f}%)".format(correct, 250, 100. * correct / 250))
 
 
 def knn(label, files):
@@ -81,13 +81,17 @@ def knn(label, files):
     preds = []
     for filename in os.listdir(directory):
         y, sr = librosa.load("test_files/" + filename, sr=None)
-        mfcc = librosa.feature.mfcc(y=y, sr=sr)
-        mfcc = normalize(mfcc.T)
+        mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13, n_fft=1024, hop_length=int(sr*0.01))
+        mfcc_d = librosa.feature.delta(mfcc)
+        mfcc = np.concatenate([mfcc, mfcc_d])
+        mfcc = normalize(mfcc)
         distance_test = []
         for i in range(len(files)):
             y1, sr1 = librosa.load(files[i], sr=None)
-            mfcc1 = librosa.feature.mfcc(y1, sr1)
-            mfcc1 = normalize(mfcc1.T)
+            mfcc1 = librosa.feature.mfcc(y=y1, sr=sr1, n_mfcc=13, n_fft=1024, hop_length=int(sr1 * 0.01))
+            mfcc_d1 = librosa.feature.delta(mfcc1)
+            mfcc1 = np.concatenate([mfcc1, mfcc_d1])
+            mfcc1 = normalize(mfcc1)
             dist = dtw(mfcc.T, mfcc1.T)
             distance_test.append(dist[-1, -1])
         new_min = np.argmin(distance_test)
@@ -96,22 +100,13 @@ def knn(label, files):
     return preds
 
 def normalize(matrix):
-    for i in range(32):
+    for i in range(26):
         avg = np.mean(matrix[i])
         matrix[i] = matrix[i] - avg
         std = np.std(matrix[i])
         matrix[i] = np.divide(matrix[i], std)
-    return matrix.T
+    return matrix
 
-def normalize_minmax(data):
-    normalized_data = []
-    for col in data.T:
-        max_val = np.max(col)
-        min_val = np.min(col)
-        rng = max_val - min_val
-        normalized_col = np.divide(col-min_val, rng)
-        normalized_data.append(normalized_col)
-    return np.array(normalized_data).T
 
 def euclidian_dist(label):
     euclidian_preds = []
@@ -121,12 +116,16 @@ def euclidian_dist(label):
     for filename in os.listdir(directory):
         results = []
         y, sr = librosa.load("test_files/" + filename, sr=None)
-        mfcc = librosa.feature.mfcc(y=y, sr=sr)
-        mfcc = normalize(mfcc.T)
+        mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13, n_fft=1024, hop_length=int(sr*0.01))
+        mfcc_d = librosa.feature.delta(mfcc)
+        mfcc = np.concatenate([mfcc, mfcc_d])
+        mfcc = normalize(mfcc)
         for j in range(len(files)):
             y1, sr1 = librosa.load(files[j], sr=None)
-            mfcc_train = librosa.feature.mfcc(y=y1, sr=sr1)
-            mfcc_train = normalize(mfcc_train.T)
+            mfcc_train = librosa.feature.mfcc(y=y1, sr=sr1, n_mfcc=13, n_fft=1024, hop_length=int(sr1 * 0.01))
+            mfcc_d1 = librosa.feature.delta(mfcc_train)
+            mfcc_train = np.concatenate([mfcc_train, mfcc_d1])
+            mfcc_train = normalize(mfcc_train)
             avg_dist = vector_euclidian(mfcc, mfcc_train)
             results.append(avg_dist)
         min_euclidian_pred = np.argmin(results)
@@ -137,7 +136,7 @@ def euclidian_dist(label):
 
 def vector_euclidian(mfcc, mfcc_train):
     dist = []
-    for i in range(19):
+    for i in range(26):
         dist.append(np.linalg.norm(mfcc[i] - mfcc_train[i]))
     return np.average(dist)
 
